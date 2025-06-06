415744c0d27b81c56b04a6a2ddc2a98ab21afce5
eunchooshin <59875661+eunchooshin@users.noreply.github.com>
2025-06-06 13:18:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,6 @@ name = st.text_input('영어로 동물을 입력하세요')
 if st.button('찾아보기'):
     url = 'https://edu.spartacodingclub.kr/random/?'+name
     st.image(url)
+
+key = st.secrets['API_KEY']
+st.write(key)
