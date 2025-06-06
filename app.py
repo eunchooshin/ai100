a9531db37ed41be76b26f8f9760f779bde92c08c
eunchooshin <59875661+eunchooshin@users.noreply.github.com>
2025-06-06 13:13:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,5 @@ st.title('동물 이미지 찾아주기 😎')
 name = st.text_input('영어로 동물을 입력하세요')
 
 if st.button('찾아보기'):
-    st.image(f'https://edu.spartacodingclub.kr/random/?{name}')
+    url = 'https://edu.spartacodingclub.kr/random/?'+name
+    st.image(url)
